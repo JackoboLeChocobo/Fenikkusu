2a8f2856da63e26b31d066bb0417253c51822dd8
Jackobo Le Chocobo <117809035+JackoboLeChocobo@users.noreply.github.com>
2024-06-19 14:58:03 +0200

    Create setup.sh

---
+++ packages/setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Packages
+# Projet Fenikkusu
+
+packfenikkusu=`zenity \
+	--width=600 --height=300 \
+	--title="Fenikkusu - Installation" \
+	--text="Choisissez la catégorie désirée. périphérique USB désirée" \
+	--list \
+	--column="Catégorié" \
+	--column="Description" \
+	"/apps-bureautique" "Logiciels pour une utilisation quotidienne" \
+	"/apps-jeux" "Logiciels pour pouvoir jouer avec vos jeux" \
+	"/apps-nvidia" "Installaion des drivers pour votre carte Nvidia" \
+	"/apps-programmation" "Logiciels pour développer vos projets informatiques" \
+	"/apps-reseau" "Logiciels concernant le réseau informatique" 2> /dev/null`
+
+if echo "$packfenikkusu" | grep '/'; then
+	/usr/bin/lxterminal -e /etc/fenikkusu/Fenikkusu/packages${packfenikkusu}.sh
+fi
